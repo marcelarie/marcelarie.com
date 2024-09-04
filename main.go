e397a872c854387e523c13030a8e3fecd1632e1f
marcelarie <googlillo@gmail.com>
2024-09-04 11:17:29 +0200

    use correct component import for templ

--- main.go
+++ main.go
@@ -3,23 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
+
+	"github.com/a-h/templ"
+	"github.com/marcelarie/marcelarie.com/hello"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
+	component := hello.Hello("Marcel")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
-	})
+	http.Handle("/", templ.Handler(component))
 
-	fmt.Printf("Starting server on port %s\n", port)
-	fmt.Printf("You can go to http://localhost:%s\n", port)
-	err := http.ListenAndServe(":"+port, nil)
-	if err != nil {
-		fmt.Printf("Error starting server: %v\n", err)
-	}
+	fmt.Println("Listening on http://localhost:3000")
+	http.ListenAndServe(":3000", nil)
 }
