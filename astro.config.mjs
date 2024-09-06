b901ee8e3442322f9fd815345ed475e4e841653e
marcelarie <googlillo@gmail.com>
2024-09-06 14:36:15 +0200

    Install tailwindcss and test it

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,9 @@ import vue from '@astrojs/vue';
 
 import mdx from '@astrojs/mdx';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), mdx()]
+  integrations: [vue(), mdx(), tailwind()]
 });
