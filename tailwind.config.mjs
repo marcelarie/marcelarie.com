806d5ea6d96b5e1fc23c8aaa1bcb59d9d9edc65e
marcelarie <googlillo@gmail.com>
2024-09-10 03:34:42 +0200

    update tailwind typography theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	theme: {},
+	theme: {
+		extend: {
+			typography: {
+				DEFAULT: {
+					css: {
+						fontFamily: "IBM Plex Mono, monospace",
+						h1: { fontWeight: "400", fontSize: "26px" },
+						h2: { fontWeight: "bold", fontSize: "14px" },
+						a: { "&:hover": { color: "#3182ce" }, fontSize: "14px" },
+					},
+				},
+			},
+		},
+	},
 	plugins: [require("@tailwindcss/typography")],
 };
