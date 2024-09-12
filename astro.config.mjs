407f88fe53e8c3ccc84e50945809d2b5c718de9f
marcelarie <googlillo@gmail.com>
2024-09-12 09:42:27 +0200

    load port from env if possible

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,18 @@ import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
+import { loadEnv } from "vite";
+
+const DEFAULT_PORT = 4321;
+
+// Load dev port from environment variables with priority
+const env = loadEnv(process.env.PORT, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    port: Number(env.PORT) || DEFAULT_PORT,
+  },
   integrations: [
     vue(),
     mdx({
@@ -15,7 +24,7 @@ export default defineConfig({
           dark: "vesper",
         },
       },
-      // gfm: false,
+      // gfm: false, // Github Flavored Markdown
     }),
     tailwind(),
   ],
