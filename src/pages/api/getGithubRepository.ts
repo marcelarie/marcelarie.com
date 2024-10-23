508d60853753d75f68cd55de0333522ff4a809f7
marcelarie <googlillo@gmail.com>
2024-10-23 22:48:22 +0200

    feat: add alternative project fetching method

--- src/pages/api/getGithubRepository.ts
+++ src/pages/api/getGithubRepository.ts
@@ -11,6 +11,18 @@ export type Language = {
   color: string;
 };
 
+export type Repo = {
+  name: string;
+  description: string;
+  html_url: string;
+};
+
+export type ReposTopicResponse = {
+  total_count: number;
+  incomplete_results: boolean;
+  items: Repo[];
+};
+
 export async function getRepoInfo(repoOwner: string, repoName: string) {
   const response = await fetch(`${ghApi}/repos/${repoOwner}/${repoName}`, {
     headers: {
@@ -23,6 +35,41 @@ export async function getRepoInfo(repoOwner: string, repoName: string) {
     throw new Error(`Failed to fetch repository info: ${response.statusText}`);
   }
 
+  const data = await response.json();
+  return data;
+}
+
+export async function getReposByTopic(
+  repoOwner: string,
+  topic: string,
+): Promise<ReposTopicResponse> {
+  const response = await fetch(
+    `${ghApi}/search/repositories?q=user:${repoOwner}+topic:${topic}&sort=updated&order=desc`,
+    {
+      headers: {
+        Authorization: `Bearer ${env.GITHUB_TOKEN}`,
+        Accept: "application/vnd.github.v3+json",
+      },
+    },
+  );
+  if (!response.ok) {
+    throw new Error(`Failed to fetch repository info: ${response.statusText}`);
+  }
+  const data = await response.json();
+  console.log(data);
+  return data.items;
+}
+
+export async function getRepos(repoOwner: string): Promise<Repo[]> {
+  const response = await fetch(`${ghApi}/users/${repoOwner}/repos`, {
+    headers: {
+      Authorization: `Bearer ${env.GITHUB_TOKEN}`,
+      Accept: "application/vnd.github.v3+json",
+    },
+  });
+  if (!response.ok) {
+    throw new Error(`Failed to fetch repository info: ${response.statusText}`);
+  }
   const data = await response.json();
   return data;
 }
@@ -55,7 +102,10 @@ export async function getRepoLanguages(
 
   const data: Record<string, number> = await response.json();
   const colorData = await colorsResponse.text();
-  const totalBytes = Object.entries(data).reduce( (acc, [_, bytes]) => acc + bytes, 0);
+  const totalBytes = Object.entries(data).reduce(
+    (acc, [_, bytes]) => acc + bytes,
+    0,
+  );
 
   const langs: Language[] = Object.entries(data).map(([lang, bytes]) => {
     const color = getSpecificRepoLanguage(colorData, lang);
