eabb3613d4569c8cf0358545504afe96748e14c6
marcelarie <googlillo@gmail.com>
2026-01-20 18:54:05 +0100

    style changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,13 +3,17 @@ export default {
   darkMode: "class", // 'media' or 'class'
   theme: {
     extend: {
-      typography: {
+      typography: (theme) => ({
         DEFAULT: {
           css: {
-            fontFamily: "IBM Plex Mono, monospace",
-            h1: { fontWeight: "400", fontSize: "26px" },
-            h2: { fontWeight: "bold", fontSize: "14px" },
-            a: { "&:hover": { color: "#3182ce" }, fontSize: "14px" },
+            fontFamily: theme("fontFamily.mono"),
+            h1: { fontWeight: "500", fontSize: "2rem" },
+            h2: { fontWeight: "400", fontSize: "1.75rem" },
+            h3: { fontWeight: "400", fontSize: "1.5rem" },
+            h4: { fontWeight: "300", fontSize: "1.25rem" },
+            h5: { fontWeight: "300", fontSize: "1.125rem" },
+            h6: { fontWeight: "300", fontSize: "1rem" },
+            a: { "&:hover": { color: "#3182ce" } },
             article: { backgroundColor: "#f7f7f7" },
             code: {
               backgroundColor: "#ffffff",
@@ -41,6 +45,14 @@ export default {
         },
         invert: {
           css: {
+            h1: { fontWeight: "500", color: "var(--color-text)" },
+            h2: { fontWeight: "400", color: "var(--color-text)" },
+            h3: { fontWeight: "400", color: "var(--color-text)" },
+            h4: { fontWeight: "300", color: "var(--color-text)" },
+            h5: { fontWeight: "300", color: "var(--color-text)" },
+            h6: { fontWeight: "300", color: "var(--color-text)" },
+            a: { color: "var(--color-link-visited-dark)" },
+            "a:hover": { color: "#ffffff" },
             code: {
               backgroundColor: "#333333",
               color: "var(--color-text)",
@@ -50,9 +62,12 @@ export default {
               backgroundColor: "transparent !important",
               color: "inherit !important",
             },
+            blockquote: {
+              color: "var(--color-text)",
+            },
           },
         },
-      },
+      }),
     },
   },
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
