4af9354ffae01e32e925504f62b4f7b61e84fe19
marcelarie <googlillo@gmail.com>
2026-02-22 14:26:34 +0100

    upgrade astro and fix giscus

--- src/config.ts
+++ src/config.ts
@@ -1,34 +1,34 @@
 export const SITE_CONFIG = {
-	title: "Unexpected cool pointer",
-	description: "This is my personal blog where I write about things I like.",
-	url: "https://followthetrace.com",
+  title: "Unexpected cool pointer",
+  description: "This is my personal blog where I write about things I like.",
+  url: "https://followthetrace.com",
 
-	author: {
-		name: "Marcel Arie",
-		username: "marcelarie",
-		email: "googlillo@example.com",
-		birthDate: new Date("1995-09-27"),
-		programmingStartDate: new Date("2020-10-10"),
-	},
+  author: {
+    name: "Marcel Arie",
+    username: "marcelarie",
+    email: "googlillo@example.com",
+    birthDate: new Date("1995-09-27"),
+    programmingStartDate: new Date("2020-10-10"),
+  },
 
-	github: {
-		username: "marcelarie",
-		repo: "marcelarie.com",
-		repoId: "MDEwOlJlcG9zaXRvcnkyODU1OTY4MzE",
-	},
+  github: {
+    username: "marcelarie",
+    repo: "followthetrace.com",
+    repoId: "MDEwOlJlcG9zaXRvcnkyODU1OTY4MzE",
+  },
 
-	giscus: {
-		category: "General",
-		categoryId: "DIC_kwDOEQXcn84CvJvn",
-	},
+  giscus: {
+    category: "General",
+    categoryId: "DIC_kwDOEQXcn84CvJvn",
+  },
 
-	projects: [
-		"tarea",
-		"git-commit-search",
-		"nu-alias-converter",
-		"sourses",
-		"rund",
-		"git-tellme",
-		"marcelarie.com",
-	],
+  projects: [
+    "tarea",
+    "git-commit-search",
+    "nu-alias-converter",
+    "sourses",
+    "rund",
+    "git-tellme",
+    "marcelarie.com",
+  ],
 } as const;
